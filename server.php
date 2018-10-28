dea21785a5bc1a59a913c7e5db9757347e0fb4d8
MSChoi <yuuhio9681@gmail.com>
2018-10-28 22:12:43 +0900

    Network Branch

--- server.php
+++ server.php
@@ -2,10 +2,28 @@
     require_once('database.php');
     
     $request_data = file_get_contents('php://input');
-    $request_json = json_decode($request_data);
+    $request_json = json_decode($request_data, true);
 
-    switch($request_json['request_type']) {
+    switch($request_json['request']) {
         case 'sign_in':
+            sign_in($request_json);
             break;
+        case 'sign_up' :
+            sign_up($request_json);
+            break;
+        case 'account_associate' :
+            account_associate($request_json);
+            break;
+        case 'transaction' :
+            transaction($request_json);
+            break;
+        case 'history' :
+            transaction_history($request_json);
+            break;
+        default :
+            $result = array();
+            $result['respond'] = 'Error';
+            $result['message'] = 'Request Type';
+            echo json_encode($result);
     }
 ?>
